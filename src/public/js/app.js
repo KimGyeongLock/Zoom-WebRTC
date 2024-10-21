d4831fbb90b84e277748db59d6b54fddbe60d764
Austin-Choi <austinsupra@gmail.com>
2024-10-21 18:20:45 +0900

    fix : No howling, good audio bitrate, deleted camera related functions

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -4,126 +4,42 @@ const socket = io();
 // 여기다가 stream을 얻어다줄건데 
 // stream은 유저의 비디오와 오디오가 항상 합쳐진 형태임.
 const myFace = document.getElementById("myFace");
-const muteBtn = document.getElementById("mute");
-const cameraBtn = document.getElementById("camera");
-const camerasSelect = document.getElementById("cameras");
-
-
 const call = document.getElementById("call");
+
+// 하울링 방지
+document.getElementById("myFace").volume = 0;
 
 call.hidden = true;
 
 let myStream;
-// 오디오 on/off 유무 tracking
-let muted = false;
-// 카메라 on/off 유무 tracking
-let cameraOff = false;
 let roomName;
 let myPeerConnection;
 
 // 실시간 채팅을 위한 DataChannel
 let myDataChannel;
 
-async function getCameras(){
-    try {
-        const devices = await navigator.mediaDevices.enumerateDevices();
-        // 디바이스 목록중에 종류가 videoinput 인 것만 가져오기
-        const cameras = devices.filter(device =>  device.kind === "videoinput");
-        const currentCamera = myStream.getVideoTracks()[0];
-        // 카메라 출력 옵션 고르게 해 주기
-        cameras.forEach(camera => {
-            const option = document.createElement("option");
-            // deviceId를 가지고 카메라 스위칭할거임
-            option.value = camera.deviceId;
-            option.innerText = camera.label;
-            // stream의 현재 카메라와 paint할 때의 카메라 option 가져오기
-            // 현재 사용하는 카메라가 현재 가능한 카메라 리스트중 선택되있게 함
-            if(currentCamera.label == camera.label){
-                option.selected = true;
-            }
-            camerasSelect.appendChild(option);
-        })
-    } catch (error) {
-        console.log(error);
-    }
-}
-
 async function getMedia(deviceId){
     const initialConstraints = {
-        audio: true,
-        video: { facingMode: "user" },
-    };
-    const cameraConstraints = {
-        audio: true,
-        video: { deviceId: { exact: deviceId }},
+        audio: {
+            echoCancellation: true,
+            noiseSuppression: true,
+            autoGainControl: false // 자동 이득 제어 비활성화
+        },
+        video: false,
     };
     try{
         // deviceId가 있으면 (선택한 기기가 있으면) 그걸로 카메라로 쓰고
         // 없거나 맨 처음에 켠 상태라면 selfie 카메라를 기준으로 가져옴
         myStream = await navigator.mediaDevices.getUserMedia(
-            deviceId? cameraConstraints : initialConstraints
+            initialConstraints
         );
         myFace.srcObject = myStream;
-        if(!deviceId){
-            await getCameras();
-        }
     }
     catch(e){
         console.log(e);
     }
     
 }
-// id 없이 호출하면 셀피 카메라를 쓰는 constraint를 생성할거임
-// getMedia();
-
-function handleMuteClick(){
-    myStream.getAudioTracks().forEach(track => {
-        track.enabled = !track.enabled;
-    });
-    if(!muted){
-        muteBtn.innerText = "Mute";
-        muted = true;
-    }
-    else{
-        muteBtn.innerText = "Unmute";
-        muted = false;
-    }
-}
-
-function handleCameraClick(){
-    myStream.getVideoTracks().forEach(track => {
-        track.enabled = !track.enabled;
-    })
-    if(!cameraOff){
-        cameraBtn.innerText = "Turn Camera On";
-        cameraOff = true;
-    }
-    else{
-        cameraBtn.innerText = "Turn Camera Off";
-        cameraOff = false;
-    }
-}
-
-// 카메라 입력 장치 바꿧을 때 
-// peer video에도 바뀌게끔 수정
-async function handleCameraChange(){
-    // 여기서 새 deviceID로 mediaStream을 생성하게 됨.
-    await getMedia(camerasSelect.value);
-    if(myPeerConnection){
-        // 그럼 여기서 저 새 deviceID로 stream 접근 가능
-        const videoTrack = myStream.getVideoTracks()[0];
-        const videoSender = myPeerConnection.getSenders()
-            .find(sender => sender.track.kind === "video");
-        console.log(videoSender);
-        // 지금 내 stream을 보내는 sender에게 아까 선택한 videoTrack으로
-        // track을 교체하고 그걸로 송출함
-        videoSender.replaceTrack(videoTrack);
-    }
-}
-
-muteBtn.addEventListener("click", handleMuteClick);
-cameraBtn.addEventListener("click", handleCameraClick);
-camerasSelect.addEventListener("input", handleCameraChange);
 
 // Welcome Form (join a room)
 const welcome = document.getElementById("welcome");
@@ -182,13 +98,14 @@ socket.on("welcome", async () => {
 // getUserMedia랑 addStream(addTrack)은 makeConnection에서 이미
 // 방에 입장할때 해 주었으므로 따로 하지 않음.
 socket.on("offer", async (offer)=> {
+    // offer를 받는 callee는 만들어진 datachannel를 받아서 사용하고
+    // 거기다가 이벤트 리스너만 달면 된다.
     myPeerConnection.addEventListener("datachannel", (e)=>{
         myDataChannel = e.channel;
         myDataChannel.addEventListener("message", (msg)=> {
             console.log(msg.data.toString());
         });
     });
-
 
     console.log("callee : offer received : ", offer);
     myPeerConnection.setRemoteDescription(offer);
@@ -242,6 +159,19 @@ function makeConnection(){
     myStream
         .getTracks()
         .forEach(track => myPeerConnection.addTrack(track, myStream));
+
+    // 오디오 트랙의 bitrate 조정 (128kbps)
+    const audioSender = myPeerConnection.getSenders().find(sender => {
+        sender.track.kind === "audio"
+    });
+    if(audioSender){
+        const param = audioSender.getParameters();
+        if(!param.encodings){
+            param.encodings = [{}]; // Encodings가 없으면 빈배열 초기화
+        }
+        param.encodings[0].maxBitrate = 128000;
+        audioSender.setParameters(param);
+    }
 }
 
 // caller, callee 둘 다의 브라우저에서 일어남
