2343d80014e8254c5a2b20c8b4e6da75d05f7e62
Austin-Choi <austinsupra@gmail.com>
2024-10-23 16:48:50 +0900

    feat : made basic STT with AWS Transcribe

--- src/server.js
+++ src/server.js
@@ -1,7 +1,14 @@
 import express from "express";
 import http from "http";
 import SocketIO from "socket.io";
+import dotenv from 'dotenv';
+import { TranscribeStreamingClient, StartStreamTranscriptionCommand } from "@aws-sdk/client-transcribe-streaming";
+import { PassThrough } from "stream";
+import { AWS } from "aws-sdk";
 
+// dotenv import하고 서버 초기화 전에 .config() 실행해야
+// 환경변수 읽을 수 있음
+dotenv.config();
 const app = express();
 
 app.set('view engine', "pug");
@@ -13,6 +20,49 @@ app.get("/*", (req, res) => res.redirect("/"));
 // 같은 서버에서 http랑 ws 서버 둘 다 돌림(같은 포트에서 처리 가능)
 const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
+
+// 오디오 데이터를 받을 스트림
+let audioStream = new PassThrough();
+
+const LanguageCode = "ko-KR";
+const MediaEncoding = "pcm";
+const MediaSampleRateHertz = "16000";
+
+async function startTranscribe() {
+    const client = new TranscribeStreamingClient({
+        region: process.env.AWS_REGION,
+        credentials: {
+            accessKeyId: process.env.AWS_ACCESS_ID,
+            secretAccessKey: process.env.AWS_SECRET_ID,
+        },
+    });
+
+    const params = {
+        LanguageCode,
+        MediaEncoding,
+        MediaSampleRateHertz,
+        AudioStream: (async function* (){
+            for await (const chunk of audioStream) {
+                yield {AudioEvent: {AudioChunk: chunk}};
+            }
+        })(),
+        EnableSpeakerDiarization: true,
+        MaxSpeakerLabels: 2,
+    };
+
+    const command = new StartStreamTranscriptionCommand(params);
+
+    const response = await client.send(command);
+
+    try {
+        for await ( const event of response.TranscriptResultStream){
+            console.log(JSON.stringify(event));
+        }
+    }
+    catch(error) {
+        console.error("transcribe error : ", error);
+    }
+}
 
 wsServer.on("connection", socket => {
     socket.on("join_room", async (roomName, email) => {
@@ -35,7 +85,13 @@ wsServer.on("connection", socket => {
 
             // 입장한 사용자의 이메일로 입장 알림을 줌
             wsServer.to(roomName).emit("notification", `${email}님이 입장하셨습니다.`);
+            // AWS Transcribe 시작
+            startTranscribe();
         }
+    });
+    socket.on("audio_chunk", (chunk) => {
+        // 클라이언트에서 받은 오디오 데이터를 audioStream에 추가
+        audioStream.write(chunk);
     });
     // caller가 offer로 보낸 sdp를 통화를 연결하려는 방에 보냄
     socket.on("offer", (offer, roomName)=>{
