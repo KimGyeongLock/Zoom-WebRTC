5c476cbff130b8d810d611710f9e417c3f7accfe
Austin-Choi <austinsupra@gmail.com>
2024-10-23 17:41:56 +0900

    chore : TODO audio-processor switch uint8arry to 16bit PCM array

--- src/public/js/audio-processor.js
+++ src/public/js/audio-processor.js
@@ -13,3 +13,12 @@ class AudioProcessor extends AudioWorkletProcessor {
 }
 
 registerProcessor('audio-processor', AudioProcessor);
+
+/*
+todo
+AudioWorklet에서 오디오 데이터를 처리할 때, 데이터를 **Uint16Array**로 변환하는 것은 올바른 접근 방식이 아닙니다. AudioWorklet에서 제공되는 오디오 데이터는 Float32Array 형식으로, -1.0에서 1.0 사이의 범위 값을 가지는 32비트 부동 소수점(32-bit float) 값입니다. 이 값을 16비트 PCM 형식으로 변환해야 합니다.
+
+따라서 Uint16Array 대신, **Int16Array**로 변환한 후 이를 **Uint8Array**로 패키징하여 서버로 전송해야 합니다.
+
+
+*/
